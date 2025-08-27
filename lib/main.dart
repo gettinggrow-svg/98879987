b3c7d1cbe0f7021071aceaf6c02eb594d7c63277
gettinggrow-svg <gettinggrow@gmail.com>
2025-08-27 19:18:58 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,117 +11,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'YouTube Mirror',
+      theme: ThemeData.dark(),
+      home: const YoutubeMirror(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        colorScheme: const ColorScheme.dark(),
-        useMaterial3: true,
-      ),
-      home: const WebViewScreen(),
     );
   }
 }
 
-class WebViewScreen extends StatefulWidget {
-  const WebViewScreen({super.key});
+class YoutubeMirror extends StatefulWidget {
+  const YoutubeMirror({super.key});
 
   @override
-  State<WebViewScreen> createState() => _WebViewScreenState();
+  State<YoutubeMirror> createState() => _YoutubeMirrorState();
 }
 
-class _WebViewScreenState extends State<WebViewScreen> {
+class _YoutubeMirrorState extends State<YoutubeMirror> {
   late final WebViewController _controller;
-
-  final List<String> _instances = <String>[
-    // Список зеркал (можно менять в коде)
-    'https://piped.video',
-    'https://piped.projectsegfau.lt',
-    'https://yt.artemislena.eu',
-    'https://invidious.snopyta.org',
-    'https://invidious.io',
-  ];
-
-  int _current = 0;
-  bool _showInsult = false; // переключатель фразы
 
   @override
   void initState() {
     super.initState();
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setBackgroundColor(const Color(0xFF000000))
-      ..setNavigationDelegate(NavigationDelegate(
-        onNavigationRequest: (request) {
-          // Разрешаем все https ссылки
-          return NavigationDecision.navigate;
-        },
-      ))
-      ..loadRequest(Uri.parse(_instances[_current]));
-  }
-
-  void _reload() {
-    _controller.reload();
-  }
-
-  void _goBack() async {
-    if (await _controller.canGoBack()) {
-      _controller.goBack();
-    }
-  }
-
-  void _goForward() async {
-    if (await _controller.canGoForward()) {
-      _controller.goForward();
-    }
-  }
-
-  void _nextInstance() {
-    setState(() {
-      _current = (_current + 1) % _instances.length;
-      _controller.loadRequest(Uri.parse(_instances[_current]));
-    });
+      ..loadRequest(
+        Uri.parse("https://piped.video"), // <- зеркало YouTube
+      );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('YouTube Mirror Viewer'),
-            if (_showInsult)
-              const Text(
-                'Лёшка G0vно3жка',
-                style: TextStyle(fontSize: 12),
-              ),
-          ],
-        ),
-        actions: [
-          IconButton(onPressed: _goBack, icon: const Icon(Icons.arrow_back)),
-          IconButton(onPressed: _goForward, icon: const Icon(Icons.arrow_forward)),
-          IconButton(onPressed: _reload, icon: const Icon(Icons.refresh)),
-          IconButton(
-            tooltip: 'Сменить зеркало',
-            onPressed: _nextInstance,
-            icon: const Icon(Icons.route),
-          ),
-          const SizedBox(width: 8),
-          Switch(
-            value: _showInsult,
-            onChanged: (v) => setState(() => _showInsult = v),
-          ),
-        ],
-      ),
-      body: SafeArea(child: WebViewWidget(controller: _controller)),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        child: Text(
-          'Зеркало: ${_instances[_current]}',
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-      ),
+      appBar: AppBar(title: const Text("YouTube Mirror")),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
